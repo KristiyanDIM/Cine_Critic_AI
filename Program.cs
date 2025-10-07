f706d1a1282eb5101aedb5309aac6170a5a65853
Music_Bingo <krischun1@gmail.com>
2025-10-07 15:14:47 +0300

    проверка

--- Program.cs
+++ Program.cs
@@ -6,12 +6,10 @@ namespace CineCritic_AI
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
