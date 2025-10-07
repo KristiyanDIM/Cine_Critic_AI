f999f3a354c176d8bdf4714d38e40003644c22ab
Denislav Mihailov <dkilimperov@gmail.com>
2025-10-07 15:52:56 +0300

    weljw

--- Program.cs
+++ Program.cs
@@ -13,10 +13,10 @@ namespace CineCritic_AI
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            //iecwuebcwcb
+            //epfhoiehfoiuhf
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
