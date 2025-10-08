47781fae65a66bac8002e7d3ae76000b1730f043
StanimirNaydenov <108525509+StanimirNaydenov@users.noreply.github.com>
2025-10-08 12:44:38 +0300

    Фиьед

--- Models/LoginViewModel.cs
+++ Models/LoginViewModel.cs
@@ -4,12 +4,18 @@ namespace Cine_Critic_AI.Models
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage = "Моля, въведете потребителско име.")]
+        [Display(Name = "Потребителско име")]
+        public string Username { get; set; }
+
         [Required(ErrorMessage = "Моля, въведете имейл.")]
         [EmailAddress(ErrorMessage = "Невалиден имейл адрес.")]
+        [Display(Name = "Имейл")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Моля, въведете парола.")]
         [DataType(DataType.Password)]
+        [Display(Name = "Парола")]
         public string Password { get; set; }
 
         [Display(Name = "Запомни ме")]
