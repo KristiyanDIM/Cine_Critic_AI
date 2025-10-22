1d44d0f987e4f9b4b53d119a6c5902dee7e99929
Кристиян Димитров <krischun78@gmail.com>
2025-10-23 00:33:16 +0300

    StatisticsController and StatisticViewModel

---
+++ Controllers/StatisticsController.cs
@@ -0,0 +1,41 @@
+﻿using Cine_Critic_AI.Models;
+using Cine_Critic_AI.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Cine_Critic_AI.Controllers
+{
+    [Authorize] // само логнати потребители да виждат статистиката
+    public class StatisticsController : Controller
+    {
+        private readonly DatabaseService _db;
+
+        public StatisticsController(DatabaseService db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var reviews = _db.GetAllReviews();
+            var movies = _db.GetAllMovies();
+
+            var stats = new StatisticsViewModel
+            {
+                TotalReviews = reviews.Count(),
+                TotalMovies = movies.Count(),
+                AverageRating = reviews.Any() ? reviews.Average(r => r.Rate) : 0,
+                TopMovie = reviews
+                    .GroupBy(r => r.MovieId)
+                    .OrderByDescending(g => g.Count())
+                    .Select(g => movies.FirstOrDefault(m => m.Id == g.Key)?.Title ?? "Неизвестен")
+                    .FirstOrDefault() ?? "Няма ревюта"
+            };
+
+            return View(stats);
+        }
+    }
+
+   
+}
---
+++ Models/StatisticViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Cine_Critic_AI.Models
+{
+    public class StatisticsViewModel
+    {
+        public int TotalReviews { get; set; }
+        public int TotalMovies { get; set; }
+        public double AverageRating { get; set; }
+        public string TopMovie { get; set; } = "";
+    }
+}
